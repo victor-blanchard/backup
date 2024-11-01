3fbc8699f1985da3927364963d523b81972f54f8
victor-blanchard <159810800+victor-blanchard@users.noreply.github.com>
2024-11-01 14:26:02 +0100

    finished version 2024

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var router = express.Router();
+const fetch = require("node-fetch");
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/year", (req, res) => {
+  res.json({ year: new Date().getFullYear().toString() });
+});
+
+module.exports = router;
